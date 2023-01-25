e5df7ade2929b40e62c7657e5a0356387600bc05
Salah eddine Elhandaoui <selhanda@e1r4p3.1337.ma>
2023-01-25 13:25:44 +0100

    edit

--- Server/Response/Response.cpp
+++ Server/Response/Response.cpp
@@ -66,8 +66,9 @@ std::string Response::handle_get_request(std::string Path, std::string contentTy
     }
     else
     {
-        res = check_request_path("/index.html") + contentType + "\r\n\r\n";
-        file_content = read_file_content("../../Error_Pages/404.html");
+        contentType = "text/html";
+        res = check_request_path(Path) + contentType + "\r\n\r\n";
+        file_content = read_file_content("/Error_Pages/404.html");
         res += file_content;
     }
     return (res);

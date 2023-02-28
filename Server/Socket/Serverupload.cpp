a2b10b3c4332748e32f9e9235e4aba91ec943006
salah <codersalah0@gmail.com>
2023-02-28 23:35:25 +0100

    edit

--- Server/Socket/Serverupload.cpp
+++ Server/Socket/Serverupload.cpp
@@ -94,3 +94,71 @@ void parse_upload_post_data(std::string http_request)
     }
 }
 }
+
+
+
+
+
+////////////////////////////////////////
+
+
+
+void Server::parse_upload_post_data(char* buffer) {
+    std::string data(buffer);
+    std::string boundary("boundary=");
+
+    // Find the boundary string in the request
+    size_t pos = data.find(boundary);
+    if (pos == std::string::npos) {
+        std::cerr << "Error: Could not find boundary in POST request" << std::endl;
+        return;
+    }
+
+    // Get the boundary value
+    std::string boundary_value = "--" + data.substr(pos + boundary.length(), 16);
+
+    // Split the request data by the boundary
+    std::vector<std::string> parts;
+    size_t prev_pos = data.find(boundary_value);
+    while (prev_pos != std::string::npos) {
+        size_t next_pos = data.find(boundary_value, prev_pos + boundary_value.length());
+        if (next_pos == std::string::npos) {
+            parts.push_back(data.substr(prev_pos));
+            break;
+        }
+        parts.push_back(data.substr(prev_pos, next_pos - prev_pos));
+        prev_pos = next_pos;
+    }
+
+    // Parse the parts
+    for (const auto& part : parts) {
+        // Find the content-type and content-disposition headers
+        std::string content_type_header("Content-Type:");
+        std::string content_disposition_header("Content-Disposition:");
+        size_t content_type_pos = part.find(content_type_header);
+        size_t content_disposition_pos = part.find(content_disposition_header);
+        if (content_type_pos == std::string::npos || content_disposition_pos == std::string::npos) {
+            continue;
+        }
+
+        // Parse the content-type and content-disposition headers
+        std::string content_type_value = part.substr(content_type_pos + content_type_header.length(), part.find("\r\n", content_type_pos) - (content_type_pos + content_type_header.length()));
+        std::string content_disposition_value = part.substr(content_disposition_pos + content_disposition_header.length(), part.find("\r\n", content_disposition_pos) - (content_disposition_pos + content_disposition_header.length()));
+
+        // Find the file name and file data in the part
+        std::string filename_header("filename=\"");
+        size_t filename_pos = content_disposition_value.find(filename_header);
+        if (filename_pos == std::string::npos) {
+            continue;
+        }
+        std::string filename_value = content_disposition_value.substr(filename_pos + filename_header.length(), content_disposition_value.find("\"", filename_pos + filename_header.length()) - (filename_pos + filename_header.length()));
+        size_t file_data_pos = part.find("\r\n\r\n") + 4;
+        std::string file_data = part.substr(file_data_pos, part.length() - file_data_pos - boundary_value.length());
+
+        // Save the file to disk
+        std::ofstream outfile(filename_value, std::ios::binary);
+        outfile.write(file_data.c_str(), file_data.length());
+        outfile.close();
+    }
+}
+
